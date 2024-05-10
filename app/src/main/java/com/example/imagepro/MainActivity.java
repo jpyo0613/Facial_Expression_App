2cdfbd7e06909cd912ce70074502e07a19f0fb92
JP <jpyo0613@gmail.com>
2024-05-10 21:54:56 +0900

    버그 수정

--- app/src/main/java/com/example/imagepro/MainActivity.java
+++ app/src/main/java/com/example/imagepro/MainActivity.java
@@ -102,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         isMainForeground = false;
+        tts.stop();
         stopGuideTTS();
     }
 
